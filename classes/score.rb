23f9d8e16ff4968f1eb8691eb448116c36d7e051
simon-tanna <gcab022109@coderacademy.edu.au>
2021-12-09 10:43:56 +1000

    created classes files game, players, teams, user_options and username. wrote and tested first 2 tests for Score class

---
+++ classes/score.rb
@@ -0,0 +1,15 @@
+class Score
+    attr_reader :score, :team
+
+    def initialize(team, score)
+        @team = team
+        @score = score
+    end
+    
+    # def team
+    # end
+    
+    # def score
+    #     return @score
+    # end
+end
--- spec/football_game_spec.rb
+++ spec/football_game_spec.rb
@@ -2,15 +2,22 @@ require_relative "../classes/game"
 require_relative "../classes/username"
 require_relative "../classes/teams"
 require_relative "../classes/players"
-require_relative "../classes/useroptions"
+require_relative "../classes/user_options"
 require_relative "../classes/score"
 
+# This is the first test created for the program.
+# It is testing the Score class
 describe Score do
-    it "it should return the user team score" do
-        user_team = "Buffon's Barons"
+    it "it should return the team score" do
+        team = "Buffon's Barons"
         score = 3
-        score_hash = Score.new(user_team,score)
-        expect(score_hash.score).to eq(score)
+        score_return = Score.new(team,score)
+        expect(score_return.score).to eq(score)
     end
-
+    it "it should return the team name" do
+        team = "Buffon's Barons"
+        score = 3
+        score_return = Score.new(team,score)
+        expect(score_return.team).to eq(team)
+    end
 end
